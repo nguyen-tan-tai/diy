81ba460427479e3d4e298fa18610f98b417803a3
ER_TaiNguyen <tai.nguyen@ever-rise.co.jp>
2023-10-06 16:06:04 +0700

    ﻿misc

--- calendar_clock/firmware/firmware.ino
+++ calendar_clock/firmware/firmware.ino
@@ -1,46 +1,145 @@
-int S74HC595_DATA_PIN = 5;
-int S74HC595_SHIFT_PIN = 7;
-int S74HC595_LATCH_PIN = 6;
+#include <Shifty.h>
 
-int digits[10][8] {
-  {0, 0, 0, 1, 0, 1, 0, 0}, // digit 0
-  {0, 1, 1, 1, 0, 1, 1, 1}, // digit 1
-  {1, 0, 0, 0, 0, 1, 0, 1}, // digit 2
-  {0, 0, 1, 0, 0, 1, 0, 1}, // digit 3
-  {0, 1, 1, 0, 0, 1, 1, 0}, // digit 4
-  {0, 0, 1, 0, 1, 1, 0, 0}, // digit 5
-  {0, 0, 0, 0, 1, 1, 0, 0}, // digit 6
-  {0, 1, 1, 1, 0, 1, 0, 1}, // digit 7
-  {0, 0, 0, 0, 0, 1, 0, 0}, // digit 8
-  {0, 0, 1, 0, 0, 1, 0, 0}  // digit 9
-};
+Shifty myreg;
+const int BIT_CNT = 16;
 
 void setup() {
-  pinMode(S74HC595_DATA_PIN, OUTPUT);
-  pinMode(S74HC595_SHIFT_PIN, OUTPUT);
-  pinMode(S74HC595_LATCH_PIN,  OUTPUT);
-  shift(8);
-  shift(5);
+  myreg.setBitCount(BIT_CNT);
+  myreg.setPins(5, 7, 6);
+  off();
 }
 
 void loop() {
-//  for (int i = 0; i < 10; i++) {
-//    display_digit(i);
-//    delay(1000);
-//  }
+  off();
+  delay(1000);
+  d(0, 1);
+  delay(1000);
+  d(1, 2);
+  delay(1000);
+  d(2, 3);
+  delay(1000);
+  d(3, 4);
+  delay(1000);
+  d(4, 5);
+  delay(1000);
+  d(5, 6);
+  delay(1000);
+  d(6, 7);
+  delay(1000);
+  d(7, 8);
+  delay(1000);
+  d(8, 9);
+  delay(1000);
+  d(9, 0);
+  delay(1000);
 }
 
-void shift(int d) {
-  digitalWrite(S74HC595_SHIFT_PIN, LOW);
-  for (int i = 7; i >= 0; i--)  {
-    digitalWrite(S74HC595_LATCH_PIN, LOW);
-    if (digits[d][i] == 1) {
-      digitalWrite(S74HC595_DATA_PIN,  LOW);
-    }
-    if (digits[d][i] == 0) {
-      digitalWrite(S74HC595_DATA_PIN, HIGH);
-    }
-    digitalWrite(S74HC595_LATCH_PIN, HIGH);
+void d(byte d1, byte d2) {
+  myreg.batchWriteBegin();
+  w(0, d2);
+  w(1, d1);
+  myreg.batchWriteEnd();
+}
+
+void off() {
+  myreg.batchWriteBegin();
+  for (byte i = 0; i < BIT_CNT; i++) {
+    myreg.writeBit(i, HIGH);
   }
-  digitalWrite(S74HC595_SHIFT_PIN, HIGH);
+  myreg.batchWriteEnd();
+}
+
+void w(byte p, byte d) {
+  byte i = p * 8;
+  if (d == 0) {
+    myreg.writeBit(i + 0, LOW);
+    myreg.writeBit(i + 1, LOW);
+    myreg.writeBit(i + 2, LOW);
+    myreg.writeBit(i + 3, HIGH);
+    myreg.writeBit(i + 4, LOW);
+    myreg.writeBit(i + 5, HIGH);
+    myreg.writeBit(i + 6, LOW);
+    myreg.writeBit(i + 7, LOW);
+  } else if (d == 1) {
+    myreg.writeBit(i + 0, LOW);
+    myreg.writeBit(i + 1, HIGH);
+    myreg.writeBit(i + 2, HIGH);
+    myreg.writeBit(i + 3, HIGH);
+    myreg.writeBit(i + 4, HIGH);
+    myreg.writeBit(i + 5, HIGH);
+    myreg.writeBit(i + 6, HIGH);
+    myreg.writeBit(i + 7, LOW);
+  } else if (d == 2) {
+    myreg.writeBit(i + 0, HIGH);
+    myreg.writeBit(i + 1, LOW);
+    myreg.writeBit(i + 2, LOW);
+    myreg.writeBit(i + 3, LOW);
+    myreg.writeBit(i + 4, HIGH);
+    myreg.writeBit(i + 5, HIGH);
+    myreg.writeBit(i + 6, LOW);
+    myreg.writeBit(i + 7, LOW);
+  } else if (d == 3) {
+    myreg.writeBit(i + 0, LOW);
+    myreg.writeBit(i + 1, LOW);
+    myreg.writeBit(i + 2, HIGH);
+    myreg.writeBit(i + 3, LOW);
+    myreg.writeBit(i + 4, HIGH);
+    myreg.writeBit(i + 5, HIGH);
+    myreg.writeBit(i + 6, LOW);
+    myreg.writeBit(i + 7, LOW);
+  } else if (d == 4) {
+    myreg.writeBit(i + 0, LOW);
+    myreg.writeBit(i + 1, HIGH);
+    myreg.writeBit(i + 2, HIGH);
+    myreg.writeBit(i + 3, LOW);
+    myreg.writeBit(i + 4, LOW);
+    myreg.writeBit(i + 5, HIGH);
+    myreg.writeBit(i + 6, HIGH);
+    myreg.writeBit(i + 7, LOW);
+  } else if (d == 5) {
+    myreg.writeBit(i + 0, LOW);
+    myreg.writeBit(i + 1, LOW);
+    myreg.writeBit(i + 2, HIGH);
+    myreg.writeBit(i + 3, LOW);
+    myreg.writeBit(i + 4, LOW);
+    myreg.writeBit(i + 5, HIGH);
+    myreg.writeBit(i + 6, LOW);
+    myreg.writeBit(i + 7, HIGH);
+  } else if (d == 6) {
+    myreg.writeBit(i + 0, LOW);
+    myreg.writeBit(i + 1, LOW);
+    myreg.writeBit(i + 2, LOW);
+    myreg.writeBit(i + 3, LOW);
+    myreg.writeBit(i + 4, LOW);
+    myreg.writeBit(i + 5, HIGH);
+    myreg.writeBit(i + 6, LOW);
+    myreg.writeBit(i + 7, HIGH);
+  } else if (d == 7) {
+    myreg.writeBit(i + 0, LOW);
+    myreg.writeBit(i + 1, HIGH);
+    myreg.writeBit(i + 2, HIGH);
+    myreg.writeBit(i + 3, HIGH);
+    myreg.writeBit(i + 4, HIGH);
+    myreg.writeBit(i + 5, HIGH);
+    myreg.writeBit(i + 6, LOW);
+    myreg.writeBit(i + 7, LOW);
+  } else if (d == 8) {
+    myreg.writeBit(i + 0, LOW);
+    myreg.writeBit(i + 1, LOW);
+    myreg.writeBit(i + 2, LOW);
+    myreg.writeBit(i + 3, LOW);
+    myreg.writeBit(i + 4, LOW);
+    myreg.writeBit(i + 5, HIGH);
+    myreg.writeBit(i + 6, LOW);
+    myreg.writeBit(i + 7, LOW);
+  } else if (d == 9) {
+    myreg.writeBit(i + 0, LOW);
+    myreg.writeBit(i + 1, LOW);
+    myreg.writeBit(i + 2, HIGH);
+    myreg.writeBit(i + 3, LOW);
+    myreg.writeBit(i + 4, LOW);
+    myreg.writeBit(i + 5, HIGH);
+    myreg.writeBit(i + 6, LOW);
+    myreg.writeBit(i + 7, LOW);
+  }
 }
